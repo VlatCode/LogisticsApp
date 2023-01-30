75c77404e05529ec22aa98cc755c45eba7adf37d
Vlatko Nikolovski <nikolovski.vlatko97@gmail.com>
2023-01-30 12:10:34 +0100

    Added data seeding for the migration

--- LogisticsApp/LogisticsApp.DataAccess/LogisticsAppDbContext.cs
+++ LogisticsApp/LogisticsApp.DataAccess/LogisticsAppDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,7 +27,16 @@ namespace LogisticsApp.DataAccess
 
             // COURIERS TABLE
             modelBuilder.Entity<Courier>()
-                .Property(x => x.CourierName);
+                .Property(x => x.CourierName)
+                .IsRequired();
+            // Data seeding
+            modelBuilder.Entity<Courier>()
+                .HasData(
+                new Courier { Id = 1, CourierName = "Cargo4You" },
+                new Courier { Id = 2, CourierName = "ShipFaster" },
+                new Courier { Id = 3, CourierName = "MaltaShip" }
+                );
+
 
             // VALIDATIONS TABLE
             modelBuilder.Entity<Validation>()
@@ -44,6 +54,16 @@ namespace LogisticsApp.DataAccess
                  .WithMany(x => x.Validations)
                  .HasForeignKey(x => x.CourierId)
                  .IsRequired();
+            // Data seeding
+            modelBuilder.Entity<Validation>()
+                .HasData(
+                new Validation { Id = 1, CourierId = 1, ValidationType = 0, From = 0, To = 20},
+                new Validation { Id = 2, CourierId = 1, ValidationType = 1, From = 0, To = 2000},
+                new Validation { Id = 3, CourierId = 2, ValidationType = 0, From = 10, To = 30},
+                new Validation { Id = 4, CourierId = 2, ValidationType = 1, From = 0, To = 1700},
+                new Validation { Id = 5, CourierId = 3, ValidationType = 0, From = 0, To = 10},
+                new Validation { Id = 6, CourierId = 3, ValidationType = 1, From = 500, To = 50000}
+                );
 
             // CALCULATIONS TABLE
             modelBuilder.Entity<Calculation>()
@@ -62,6 +82,27 @@ namespace LogisticsApp.DataAccess
                 .HasOne(x => x.Courier)
                 .WithMany(x => x.Calculations)
                 .HasForeignKey(x => x.CourierId);
+            // Data seeding
+            modelBuilder.Entity<Calculation>()
+                .HasData(
+                new Calculation { Id = 1, CourierId = 1, CalculationType = 0, From = 0, To = 2, Cost = 15 },
+                new Calculation { Id = 2, CourierId = 1, CalculationType = 0, From = 2, To = 15, Cost = 18 },
+                new Calculation { Id = 3, CourierId = 1, CalculationType = 0, From = 15, To = 20, Cost = 35 },
+                new Calculation { Id = 4, CourierId = 1, CalculationType = 1, From = 0, To = 1000, Cost = 10 },
+                new Calculation { Id = 5, CourierId = 1, CalculationType = 1, From = 1000, To = 2000, Cost = 20 },
+                new Calculation { Id = 6, CourierId = 2, CalculationType = 0, From = 10, To = 15, Cost = 16.5 },
+                new Calculation { Id = 7, CourierId = 2, CalculationType = 0, From = 15, To = 25, Cost = 36.5 },
+                new Calculation { Id = 8, CourierId = 2, CalculationType = 0, From = 25, To = 1000, Cost = 40 },
+                new Calculation { Id = 9, CourierId = 2, CalculationType = 1, From = 0, To = 1000, Cost = 11.99 },
+                new Calculation { Id = 10, CourierId = 2, CalculationType = 1, From = 1000, To = 1700, Cost = 21.99 },
+                new Calculation { Id = 11, CourierId = 3, CalculationType = 0, From = 10, To = 20, Cost = 16.99 },
+                new Calculation { Id = 12, CourierId = 3, CalculationType = 0, From = 20, To = 30, Cost = 33.99 },
+                new Calculation { Id = 13, CourierId = 3, CalculationType = 0, From = 30, To = 1000, Cost = 43.99 },
+                new Calculation { Id = 14, CourierId = 3, CalculationType = 1, From = 0, To = 1000, Cost = 9.5 },
+                new Calculation { Id = 15, CourierId = 3, CalculationType = 1, From = 1000, To = 2000, Cost = 19.5 },
+                new Calculation { Id = 16, CourierId = 3, CalculationType = 1, From = 2000, To = 5000, Cost = 48.5 },
+                new Calculation { Id = 17, CourierId = 3, CalculationType = 1, From = 5000, To = 50000, Cost = 147.5 }
+                );
 
             // PACKAGES TABLE
             modelBuilder.Entity<Package>()
---
+++ LogisticsApp/LogisticsApp.DataAccess/Migrations/20230130105901_initial.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LogisticsApp.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Couriers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourierName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Couriers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Calculations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourierId = table.Column<int>(type: "int", nullable: false),
+                    CalculationType = table.Column<int>(type: "int", nullable: false),
+                    From = table.Column<int>(type: "int", nullable: false),
+                    To = table.Column<int>(type: "int", nullable: false),
+                    Cost = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Calculations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Calculations_Couriers_CourierId",
+                        column: x => x.CourierId,
+                        principalTable: "Couriers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Validations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourierId = table.Column<int>(type: "int", nullable: false),
+                    ValidationType = table.Column<int>(type: "int", nullable: false),
+                    From = table.Column<int>(type: "int", nullable: false),
+                    To = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Validations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Validations_Couriers_CourierId",
+                        column: x => x.CourierId,
+                        principalTable: "Couriers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Packages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Weight = table.Column<int>(type: "int", nullable: false),
+                    Dimensions = table.Column<int>(type: "int", nullable: false),
+                    CalculationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Packages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Packages_Calculations_CalculationId",
+                        column: x => x.CalculationId,
+                        principalTable: "Calculations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Couriers",
+                columns: new[] { "Id", "CourierName" },
+                values: new object[,]
+                {
+                    { 1, "Cargo4You" },
+                    { 2, "ShipFaster" },
+                    { 3, "MaltaShip" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Calculations",
+                columns: new[] { "Id", "CalculationType", "Cost", "CourierId", "From", "To" },
+                values: new object[,]
+                {
+                    { 1, 0, 15.0, 1, 0, 2 },
+                    { 2, 0, 18.0, 1, 2, 15 },
+                    { 3, 0, 35.0, 1, 15, 20 },
+                    { 4, 1, 10.0, 1, 0, 1000 },
+                    { 5, 1, 20.0, 1, 1000, 2000 },
+                    { 6, 0, 16.5, 2, 10, 15 },
+                    { 7, 0, 36.5, 2, 15, 25 },
+                    { 8, 0, 40.0, 2, 25, 1000 },
+                    { 9, 1, 11.99, 2, 0, 1000 },
+                    { 10, 1, 21.989999999999998, 2, 1000, 1700 },
+                    { 11, 0, 16.989999999999998, 3, 10, 20 },
+                    { 12, 0, 33.990000000000002, 3, 20, 30 },
+                    { 13, 0, 43.990000000000002, 3, 30, 1000 },
+                    { 14, 1, 9.5, 3, 0, 1000 },
+                    { 15, 1, 19.5, 3, 1000, 2000 },
+                    { 16, 1, 48.5, 3, 2000, 5000 },
+                    { 17, 1, 147.5, 3, 5000, 50000 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Validations",
+                columns: new[] { "Id", "CourierId", "From", "To", "ValidationType" },
+                values: new object[,]
+                {
+                    { 1, 1, 0, 20, 0 },
+                    { 2, 1, 0, 2000, 1 },
+                    { 3, 2, 10, 30, 0 },
+                    { 4, 2, 0, 1700, 1 },
+                    { 5, 3, 0, 10, 0 },
+                    { 6, 3, 500, 50000, 1 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Calculations_CourierId",
+                table: "Calculations",
+                column: "CourierId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Packages_CalculationId",
+                table: "Packages",
+                column: "CalculationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Validations_CourierId",
+                table: "Validations",
+                column: "CourierId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Packages");
+
+            migrationBuilder.DropTable(
+                name: "Validations");
+
+            migrationBuilder.DropTable(
+                name: "Calculations");
+
+            migrationBuilder.DropTable(
+                name: "Couriers");
+        }
+    }
+}
